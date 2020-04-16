7f1ca9c0488c8a0d44646eea41fdcb89c53b34a4
baden119 <35713761+baden119@users.noreply.github.com>
2020-04-16 11:40:44 +1000

    version 1.8

--- app.py
+++ app.py
@@ -5,21 +5,50 @@ import matplotlib.pyplot as plt, mpld3
 from matplotlib.ticker import MaxNLocator
 import numpy as np
 import requests
+# For testing only
+import time
 
 app = Flask(__name__)
 
-def term_counter(current_query, before, after, sub):
-    # Accepts datetime before and after, and string query and sub. converts datetime to timestamp, feeds all info to pushshift and
-    # returns a count of occurances of the query in the sub over that time.
-    before = int(before.timestamp())
-    after = int(after.timestamp())
-    url = 'https://api.pushshift.io/reddit/search/submission/?title='+str(current_query)+'&after='+str(after)+'&before='+str(before)+'&subreddit='+str(sub)+'&fields=title'+'&size=1000'
+def term_counter(subs_string, before, after, search_term, sub_count):
+    # Accepts datetime before and after, list of subs and a search term. Passes this info to pushshift API and
+    # returns a list of the number of occurances of the search term in each sub over that time period.
+    t0 = time.time()
+    term_count = []
+    url = 'https://api.pushshift.io/reddit/submission/search/?subreddit='+subs_string+'&aggs=subreddit&q='+ search_term +'&size=0&after='+str(after)+'&before='+str(before)
     r = requests.get(url)
     if not r:
         print ("Termchecker error.")
-        return render_template("error.html")
+        return False
     data = json.loads(r.text)
-    return len(data['data'])
+    t1 = time.time()
+    data = data['aggs']['subreddit']
+
+    # Creating a list of 0 values if API returns no data.
+    if (len(data)) == 0:
+        for i in range(sub_count):
+            term_count.append(0)
+
+    # Adding 0 values to list if API only returns partial data.
+    elif ((len(data) > 0) and (len(data) != sub_count)):
+        subs_list = subs_string.split(",")
+        for i in range(len(subs_list)):
+            matched = False
+            for j in range(len(data)):
+                if ((subs_list[i]).lower() == (data[j]['key']).lower()):
+                    term_count.append(data[j]['doc_count'])
+                    matched = True
+                    break
+            if matched == False:
+                term_count.append(0)
+
+    # Adding data to a list if API retuens all data.
+    else:
+        for i in range(len(data)):
+            term_count.append(data[i]['doc_count'])
+
+    print ("{0:.2f}".format(t1 - t0))
+    return(term_count)
 
 @app.route('/', methods = ["GET", "POST"])
 
@@ -31,28 +60,24 @@ def index():
         term_period = request.form.get("term_period")
         start_date = dt.datetime.strptime(request.form.get("start_date"), '%Y-%m-%d')
         end_date = dt.datetime.strptime(request.form.get("end_date"), '%Y-%m-%d')
-        sub = request.form.get("subreddit")
+        search_term = request.form.get("searchTerm")
         period_data = []
         period_lables = []
-        queries = []
-        query_count = 0
+        subs = []
+        sub_count = 0
+        sub_data = []
 
-        # For testing
-        print ('XxX Heroku Log Test XxX')
+        # Creating a list of Subreddits from HTML form, number of subs requested by user.``
+        sub = request.form.get('subreddit'+str(sub_count))
+        while sub:
+            subs.append(sub)
+            sub_count += 1
+            sub = request.form.get('subreddit'+str(sub_count))
+        subs_string  = ','.join(map(str, subs))
 
-        # Create queries list from HTML
-        query = request.form.get('searchTerm'+str(query_count))
-
-        # Error checking
-        if not query or not sub or not start_date or not end_date or (end_date < start_date):
-            return render_template("error.html")
-
-
-        while query:
-            queries.append(query)
-            period_data.append([])
-            query_count += 1
-            query = request.form.get('searchTerm'+str(query_count))
+        # Error checking HTML Inputs. Most should be caught by JavaScript.
+        if not subs_string or not search_term or not start_date or not end_date or (end_date < start_date):
+            return render_template("error.html", error="Invalid inputs detected.")
 
         # Gathering data for graphing. Some vairables dependant on day/week/month periods requested by user.
         if term_period == ('day'):
@@ -62,8 +87,10 @@ def index():
             current_end = current_start + relativedelta(days=+1)
             while (current_end <= end_date):
                 period_lables.append(str(current_start.day) + '/' + str(current_start.month))
-                for i in range(len(queries)):
-                    period_data[i].append(term_counter(current_query = queries[i], before = current_end, after = current_start, sub = sub))
+                push_data = (term_counter(subs_string = subs_string, before = current_end, after = current_start, search_term = search_term, sub_count = sub_count))
+                if not push_data:
+                    return render_template("error.html", error="Invalid Subreddit.")
+                else: sub_data.append(push_data)
                 current_start = current_end
                 current_end = current_start + relativedelta(days=+1)
 
@@ -78,8 +105,10 @@ def index():
             current_end = current_start + relativedelta(days=+7)
             while (current_end <= end_date):
                 period_lables.append(str(current_start.day) + '/' + str(current_start.month))
-                for i in range(len(queries)):
-                    period_data[i].append(term_counter(current_query = queries[i], before = current_end, after = current_start, sub = sub))
+                push_data = (term_counter(subs_string = subs_string, before = current_end, after = current_start, search_term = search_term, sub_count = sub_count))
+                if not push_data:
+                    return render_template("error.html", error="Invalid Subreddit.")
+                else: sub_data.append(push_data)
                 current_start = current_end
                 current_end = current_start + relativedelta(days=+7)
 
@@ -92,13 +121,25 @@ def index():
             current_end = current_start + relativedelta(day=31)
             while (current_end <= end_date):
                 period_lables.append(str(current_start.month) + '/' + str(current_start.year))
-                for i in range(len(queries)):
-                    period_data[i].append(term_counter(current_query = queries[i], before = current_end, after = current_start, sub = sub))
+                push_data = (term_counter(subs_string = subs_string, before = current_end, after = current_start, search_term = search_term, sub_count = sub_count))
+                if not push_data:
+                    return render_template("error.html", error="Invalid Subreddit.")
+                else: sub_data.append(push_data)
                 current_start += relativedelta(months=+1)
                 current_end += relativedelta(months=+1)
                 current_end += relativedelta(day=31)
 
-        # Graphing Data
+        # Rearranging data for graphing
+        for i in range(len(subs)):
+            period_data.append([])
+
+        for i in range(len(sub_data)):
+            for j in range(len(period_data)):
+                period_data[j].append(sub_data[i][j])
+
+
+        ### Graphing Data ###
+
         xpos = np.arange(len(period_lables))
 
         # Setting graph size and background colour
@@ -107,8 +148,8 @@ def index():
         ax.set_facecolor("#fff44f")
         ax.yaxis.set_major_locator(MaxNLocator(integer=True))
 
-        # Dynamically set width of bars depending on number of queries
-        number_of_bars = len(queries)
+        # Dynamically set width of bars depending on number of subs
+        number_of_bars = len(subs)
         bar_width = round(0.9 / number_of_bars, 3)
         xpos_current = (round(- 0.4 + (bar_width / 2), 3))
         width_values = []
@@ -120,33 +161,29 @@ def index():
 
         # Plot data from Pushshift API
         for i in range(number_of_bars):
-            plt.bar(xpos+ width_values[i], period_data[i], width = bar_width, label = queries[i])
+            plt.bar(xpos+ width_values[i], period_data[i], width = bar_width, label = subs[i])
 
         # Labelling axis
         plt.xticks(xpos, period_lables)
         plt.xlabel(xlabel)
         plt.ylabel("Post Submissions")
-        plt.title('Subreddit:\n/r/' + sub)
+        plt.title('Search Term:\n' + search_term)
         plt.legend()
-
 
         html_graph = mpld3.fig_to_html(fig, template_type='simple')
 
         return render_template('squash.html', html_graph = html_graph)
-
     else:
         return render_template("index.html")
 
 @app.route("/check", methods=["GET"])
+# Route for AJAX check to warn if process time will result in a timeout error.
 def check():
 
     start_date = dt.datetime.strptime(request.args.get("start_date"), '%Y-%m-%d')
     end_date = dt.datetime.strptime(request.args.get("end_date"), '%Y-%m-%d')
-    field_count = request.args.get("field_count")
     term_period = request.args.get("term_period")
     period_count = 0
-    #Pushift query process time (in seconds) determined after rigerous testing.
-    query_process = 1.3
 
     if term_period == ('day'):
         end_date = end_date + relativedelta(days=+1)
@@ -182,32 +219,17 @@ def check():
             current_end += relativedelta(months=+1)
             current_end += relativedelta(day=31)
 
-    query_in_seconds = (period_count * query_process * int(field_count))
-
-    query_in_datetime = dt.timedelta(seconds=query_in_seconds)
-
-    if (query_in_datetime.seconds <= 10):
+    if (period_count <= 7):
         return jsonify(False)
 
-    elif ((query_in_datetime.seconds > 10) and (query_in_datetime.seconds <= 30)):
-        return ("SQuASH process time estimated at under 30sec.")
-
-    elif ((query_in_datetime.seconds > 30) and (query_in_datetime.seconds <= 60)):
-        return ("SQuASH process time estimated at under 1min.")
-
-    elif ((query_in_datetime.seconds > 60) and (query_in_datetime.seconds <= 180)):
-        return ("SQuASH process time estimated at under 3min.")
-
     else:
-        return ("Long wait time expected, consider re-evaluating request.    "  + (str(query_in_datetime)))
+        return ("Unfortunatly, SQuASH processing time for this\nrequest is such that timeout failure very likley.\n      Continue?")
 
 
 @app.route('/documentation', methods = ["GET"])
-
+# Route for Documentation page.
 def docs():
-
     return render_template("documentation.html")
-
 
 if __name__ == '__main__':
     app.run()
